5f9a02c2e57cdc82c0f5796134a1b8fa3f4a01c7
shanchunyang0919 <71080192+shanchunyang0919@users.noreply.github.com>
2026-06-24 11:28:38 -0700

    Prepare ioc-cfn-mas-client-lib for open source release (#1)
    ## Summary
    - Spell out acronyms (IOC = Internet of Cognition, CFN = Cognition Fabric Node) in README titles and first uses
    - Add Apache 2.0 license section to README
    - Add Cisco copyright headers to all source files
    - Add CODE_OF_CONDUCT.md

--- examples/sidecar/istio/agent_a.py
+++ examples/sidecar/istio/agent_a.py
@@ -1,3 +1,7 @@
+# Copyright 2026 Cisco Systems, Inc. and its affiliates
+#
+# SPDX-License-Identifier: Apache-2.0
+
 #!/usr/bin/env python3
 """Agent A - A2A server + client (NO sidecar awareness)."""
 
